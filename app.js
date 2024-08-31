37c0c82a559debebbb51fc3bbc60b3a4d14152fc
polinmi <jeonmay7@gmail.com>
2024-08-31 09:09:43 +0800

    Bday

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log ("Hello Pau!"); 
-console.log ("Dear Diary Alam mo diko alaaaaaam!!!");
+//input
+const person1 = {
+    name: "polinmi",
+    age: 20,
+    occupation: "student",
+}
+
+//process and output
+    console.log(person1.name);
+    console.log(person1.age);
+    console.log(person1.occupation);
